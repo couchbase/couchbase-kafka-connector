601499d2a04993f65b28ff0b9334e107e120d101
Sergey Avseyev <sergey.avseyev@gmail.com>
2015-11-06 08:27:28 +0300

    Update to recent couchbase core
    Change-Id: Ie8e7ba8c6cfe34d9d60a856e1ebf98ae3e22f533
    Reviewed-on: http://review.couchbase.org/56734
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/main/java/com/couchbase/kafka/CouchbaseReader.java
+++ src/main/java/com/couchbase/kafka/CouchbaseReader.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2015 Couchbase, Inc.
- * <p/>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p/>
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * <p/>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,14 +34,11 @@ import com.couchbase.client.core.message.cluster.GetClusterConfigResponse;
 import com.couchbase.client.core.message.cluster.OpenBucketRequest;
 import com.couchbase.client.core.message.cluster.SeedNodesRequest;
 import com.couchbase.client.core.message.dcp.DCPRequest;
-import com.couchbase.client.core.message.dcp.FailoverLogEntry;
 import com.couchbase.client.core.message.dcp.SnapshotMarkerMessage;
-import com.couchbase.client.core.message.dcp.StreamRequestResponse;
 import com.couchbase.client.deps.com.lmax.disruptor.EventTranslatorOneArg;
 import com.couchbase.client.deps.com.lmax.disruptor.RingBuffer;
 import com.couchbase.kafka.state.RunMode;
 import com.couchbase.kafka.state.StateSerializer;
-import rx.Observable;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
@@ -165,27 +162,7 @@ public class CouchbaseReader {
                         }
                     }
                 });
-        streamAggregator.open(state)
-                .flatMap(new Func1<StreamRequestResponse, Observable<DCPRequest>>() {
-                    @Override
-                    public Observable<DCPRequest> call(StreamRequestResponse response) {
-                        final BucketStreamState initialState = state.get(response.partition());
-                        FailoverLogEntry mostRecentEntry = null;
-                        for (FailoverLogEntry failoverLogEntry : response.failoverLog()) {
-                            if (mostRecentEntry == null || failoverLogEntry.sequenceNumber() > mostRecentEntry.sequenceNumber()) {
-                                mostRecentEntry = failoverLogEntry;
-                            }
-                        }
-                        state.put(new BucketStreamState(
-                                response.partition(),
-                                mostRecentEntry == null ? initialState.vbucketUUID() : mostRecentEntry.vbucketUUID(),
-                                mostRecentEntry == null ? initialState.startSequenceNumber() : mostRecentEntry.sequenceNumber(),
-                                initialState.endSequenceNumber(),
-                                initialState.snapshotStartSequenceNumber(),
-                                initialState.snapshotEndSequenceNumber()), false);
-                        return response.stream();
-                    }
-                })
+        streamAggregator.feed(state)
                 .toBlocking()
                 .forEach(new Action1<DCPRequest>() {
                     @Override
